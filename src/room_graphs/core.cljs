c00add69e3fd63f663faa96fcaa814ab2004d31a
J David Eisenberg <jdavid.eisenberg@gmail.com>
2016-06-18 11:48:26 -0700

    Change data to look like JSON; tighten up code.

--- src/room_graphs/core.cljs
+++ src/room_graphs/core.cljs
@@ -8,7 +8,7 @@
               [hiccups.runtime :as hiccupsrt]
               [clojure.browser.repl :as repl]))
 
-;; (defonce conn (repl/connect "http://localhost:9000/repl"))
+(defonce conn (repl/connect "http://localhost:9000/repl"))
 
 (enable-console-print!)
 
@@ -17,46 +17,33 @@
   vectors; e.g. (day-times #{} [1,3] [200 400]) ->
   #{[1 200 400][3 200 400]}"
   [times days start-time end-time]
-  (apply conj times (for [d days] (vector d start-time end-time))))
+  (into times (for [d days] (vector d start-time end-time))))
 
 (defn process-section
   "Function called by reduce; creates a map with the
   room name as key and a set of day-times as value.
   Times are fifteen-minute intervals with 0 being 7 a.m."
-  [acc item]
-  (let [[_ _ _ _ _ day-string start end room _ _ building] item
-        days (utils/to-day-list day-string)
-        start-time (quot (- (utils/to-minutes start) (* 7 60)) 15)
-        end-time (quot (- (+ 14 (utils/to-minutes end)) (* 7 60)) 15)
-        info (get acc room)]
-    (if info
-     (assoc acc room (day-times info days start-time end-time))
-     (assoc acc room (day-times #{} days start-time end-time)))))
+  [acc section-item]
+  (let [{:keys [days start-time end-time room building]} section-item
+        day-list (utils/to-day-list days)
+        start-minutes (quot (- (utils/to-minutes start-time) (* 7 60)) 15)
+        end-minutes (quot (- (+ 14 (utils/to-minutes end-time)) (* 7 60)) 15)
+        acc-times (get acc room)]
+    (if acc-times
+     (assoc acc room (day-times acc-times day-list start-minutes end-minutes))
+     (assoc acc room (day-times #{} day-list start-minutes end-minutes)))))
 
 (defn make-svg
-  "Create SVG rectangles for the given set of day-times"
-  [timeset]
- (str "<svg width='380' height='135'>"
-  (loop [t timeset
-         result ""]
-    (if (empty? t) result
-     (let [[day start end] (first t)
-           x (+ 20 (* start 5))
-           y (* day 15)
-           w (* 5 (- end start))]
-      (recur (rest t) (str result "\n<rect x='" x "' y='" y "'
-                        width='" w "' height='15'/>")))))
-  "<use xlink:href='#grid'></svg>"))
-
-
-(defn display-row
-   "Create a table row with the room
-   name in the left cell and the grid of
-   times and dates in the right cell"
-   [item info]
-   (html
-    [:tr [:td [:span.room item]]
-     [:td (make-svg info)]]))
+ "Create SVG rectangles for the given set of day-times"
+ [timeset]
+ (html
+   [:svg {:width 380 :height 135}]
+   (map (fn [[day start end]])
+     (let [x (+ 20 (* start 5))])
+     y (* day 15)
+     w (* 5 (- end start))
+     [:rect {:x x :y y :width w :height 15}])) timeset
+     [:use {:xlink:href "#grid"}])
 
 (defn display-sections
   "Create a table with one row for each room. The key for
@@ -64,14 +51,10 @@
   [section-map]
   (let [key-list (sort (keys section-map))]
       (html
-          [:table
-              (loop [k key-list ;; loop through room names
-                     result ""] ;; accumulating an HTML/SVG string
-                (if (empty? k)
-                 result
-                 (let [key1 (first k)]
-                  (recur (rest k) (str result (display-row key1
-                                               (get section-map key1)))))))])))
+          (into [:table]
+              (for [k key-list]
+                [:tr [:td [:span.room k]]
+                 [:td (make-svg (get section-map k))]])))))
 
 (defn display-section-map [section-map]
     (let [section-html (display-sections section-map)]
@@ -83,7 +66,7 @@
   all courses for the chosen building that have a non-null start time"
   [evt]
   (let [building (forms/getValue (dom/getElement "building"))
-        building-sections (filter (fn [item] (and (= (get item 11) building) (not= "" (get item 6))))
+        building-sections (filter (fn [item] (and (= (:building item) building) (not= "" (:start-time item))))
                            data/section-list)
         section-map (reduce process-section {} building-sections)]
        (if (= building "")
